286c41b8ea9dd343a101965004b516e207e928de
oksmartypants <cep2141@columbia.edu>
2015-10-28 15:35:14 -0400

    Send warnings to bit bucket

--- q3.py
+++ q3.py
@@ -1,10 +1,11 @@
 import sys
 import os
 
+
 path=sys.argv[1]
 
 print "Plotting cumulative nucleotides sequenced as a function of time for 'failed' reads"
-os.system("poretools yield_plot --plot-type basepairs --saveas cumnucfail.png '"+path+"downloads/fail/'")
+os.system("poretools yield_plot --plot-type basepairs --saveas cumnucfail.png '"+path+"downloads/fail/' &>/dev/null")
 
 print "Plotting cumulative nucleotides sequenced as a function of time for 'passed' reads"
-os.system("poretools yield_plot --plot-type basepairs --saveas cumnucpass.png '"+path+"downloads/pass/'")
+os.system("poretools yield_plot --plot-type basepairs --saveas cumnucpass.png '"+path+"downloads/pass/' &>/dev/null")
