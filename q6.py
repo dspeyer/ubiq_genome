b168e219a3f33d182fea2de4e29d8c9c703dcdb7
Daniel Speyer <dspeyer@gmail.com>
2015-10-28 03:26:28 -0400

    rewrite build system to cache

--- q6.py
+++ q6.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python
 import sys
 import os
 
 path=sys.argv[1]
 
-print "Creating 1D & 2D failure histogram"
+print "Creating 1D and 2D failure histogram"
 os.system("poretools hist "+path+"downloads/fail/")
-print "Creating 1D & 2D pass histogram"
+print "Creating 1D and 2D pass histogram"
 os.system("poretools hist "+path+"downloads/pass/")
