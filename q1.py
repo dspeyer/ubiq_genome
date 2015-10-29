b6de3259110d4b1e177b79480a858c0d364608d6
oksmartypants <cep2141@columbia.edu>
2015-10-29 15:58:57 -0400

    made q1 prettier

--- q1.py
+++ q1.py
@@ -2,32 +2,23 @@
 import sys
 import os
 
+def rr(runtype, path, passfail, typ, fp):
+	os.system("poretools fastq --type "+runtype+" "+path+"downloads/"+passfail+"/ >tempholderfastqfile.txt")
+	data = open('tempholderfastqfile.txt').read()
+	allcount = data.count('.fast5')
+	print "Number of "+typ+" reads classified as "+fp+": "+str(allcount)
+	os.remove('tempholderfastqfile.txt')
+	return allcount
+
 path=sys.argv[1]
 
-os.system("poretools fastq --type all "+path+"downloads/fail/ >tempholderfastqfile.txt")
-data = open('tempholderfastqfile.txt').read()
-allfailcount = data.count('.fast5')
-print "Number of 1D and 2D reads classified as failed: "+str(allfailcount)
-os.remove('tempholderfastqfile.txt')
-
-os.system("poretools fastq --type all "+path+"downloads/pass/ >tempholderfastqfile.txt")
-data = open('tempholderfastqfile.txt').read()
-allpasscount = data.count('.fast5')
-print "Number of 1D and 2D reads classified as passed: "+str(allpasscount)
-os.remove('tempholderfastqfile.txt')
-
-os.system("poretools fastq --type 2D "+path+"downloads/fail/ >tempholderfastqfile.txt")
-data = open('tempholderfastqfile.txt').read()
-twodfailcount = data.count('.fast5')
-os.remove('tempholderfastqfile.txt')
-
-os.system("poretools fastq --type 2D "+path+"downloads/pass/ >tempholderfastqfile.txt")
-data = open('tempholderfastqfile.txt').read()
-twodpasscount = data.count('.fast5')
-os.remove('tempholderfastqfile.txt')
+allfailcount=rr("all", path, "fail", "1D and 2D", "failed")
+allpasscount=rr("all", path, "pass", "1D and 2D", "passed")
+twodfailcount=rr("2D", path, "fail", "2D", "failed")
+twodpasscount=rr("2D", path, "pass", "2D", "passed")
 
 fractwodpass=float(twodpasscount)/float(allpasscount)
 fractwodfail=float(twodfailcount)/float(allfailcount)
 
-print "Fraction of 2D reads in passed folder: "+str(fractwodpass)
 print "Fraction of 2D reads in failed folder: "+str(fractwodfail)
+print "Fraction of 2D reads in passed folder: "+str(fractwodpass)
