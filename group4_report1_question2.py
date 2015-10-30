85ab7032257f5432263f67d5376482eeb6f380cf
Daniel Speyer <dspeyer@gmail.com>
2015-10-30 00:34:30 -0400

    verbosity in q2

--- group4_report1_question2.py
+++ group4_report1_question2.py
@@ -19,25 +19,26 @@ for i in glob(dir+'/pass/*.fast5')+glob(dir+'/fail/*.fast5'):
     cnt[ch]+=1
 
 
-sum=0
+total=0
 nch=0
 max=-1
 argmax=-1
 for i in cnt:
     nch+=1
-    sum+=cnt[i]
+    total+=cnt[i]
     if cnt[i]>max:
         max=cnt[i]
         argmax=i
 
-print "%d channels had at least one read. " % nch
-print "The average channel had %.1f reads. " % (float(sum)/nch)
+print "%d channels had at least one read, and %d had at least five.  " % (nch, sum([x>=5 for x in cnt.values()]))
+print "This compares with 434 ``active'' channels during initialization, and 651 immediately after loading fuel\n"
+print "The average channel had %.1f reads. " % (float(total)/nch)
 print "Channel %s had %d reads, which was the most." % (argmax, max)
 
-plot.clf()
-plot.hist(cnt.values(), bins=20)
-plot.xlabel('Reads per Channel')
-plot.ylabel('Channels with that many Reads')
-plot.savefig('part2hist.eps',format='eps')
+# plot.clf()
+# plot.hist(cnt.values(), bins=20)
+# plot.xlabel('Reads per Channel')
+# plot.ylabel('Channels with that many Reads')
+# plot.savefig('part2hist.eps',format='eps')
 
 print "\nJust for fun, here's a histogram of reads per channel\\\\"
